6ca4c759a8a1e5169b9c78774eb3cdf3fe78bced
Carelle9000 <ornellatiako730@gmail.com>
2025-10-07 11:04:45 +0100

    Amelioration du module ecommerce

--- backend/src/ecommerce/taxes/taxes.module.ts
+++ backend/src/ecommerce/taxes/taxes.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TaxesService } from './taxes.service';
 import { TaxesController } from './taxes.controller';
+import { PrismaService } from 'src/common/utils/prisma/prisma.service';
 
 @Module({
-  providers: [TaxesService],
+  providers: [TaxesService,PrismaService],
   controllers: [TaxesController]
 })
 export class TaxesModule {}
