80b0eb24a9f21ab970ae33487792018fbb32c3a5
Nanfack95 <nanfackjospinduclair@gmail.com>
2025-10-08 15:54:32 +0100

    Resolve Error

--- backend/src/purchase/purchase-orders/purchase-orders.module.ts
+++ backend/src/purchase/purchase-orders/purchase-orders.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PurchaseOrdersController } from './purchase-orders.controller';
 import { PurchaseOrdersService } from './purchase-orders.service';
+import { UtilsModule } from 'src/common/utils/utils.module';
+import { PrismaService } from 'src/common/utils/prisma/prisma.service';
 
 @Module({
+  imports: [UtilsModule],
   controllers: [PurchaseOrdersController],
-  providers: [PurchaseOrdersService]
+  providers: [PurchaseOrdersService, PrismaService]
 })
 export class PurchaseOrdersModule {}
--- backend/src/purchase/suppliers/suppliers.module.ts
+++ backend/src/purchase/suppliers/suppliers.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { SuppliersController } from './suppliers.controller';
 import { SuppliersService } from './suppliers.service';
+import { UtilsModule } from 'src/common/utils/utils.module';
+import { PrismaService } from 'src/common/utils/prisma/prisma.service';
 
 @Module({
+  imports: [UtilsModule],
   controllers: [SuppliersController],
-  providers: [SuppliersService]
+  providers: [SuppliersService, PrismaService]
 })
 export class SuppliersModule {}
