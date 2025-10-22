671ff124194c5f9d47cb4ffec8849aa73f412c97
Carelle9000 <ornellatiako730@gmail.com>
2025-10-22 21:26:58 +0100

    Correction de l'orthographe du module interaction

--- backend/src/crm/crm.module.ts
+++ backend/src/crm/crm.module.ts
@@ -4,10 +4,12 @@ import { AccountsModule } from './accounts/accounts.module';
 import { ContactsModule } from './contacts/contacts.module';
 import { OpportunitiesModule } from './opportunities/opportunities.module';
 import { CrmtasksModule } from './crmtasks/crmtasks.module';
-import { InteractionsModule } from './interractions/interractions.module';
+import { InteractionsModule } from './interactions/interactions.module';
 import { ContractsModule } from './contracts/contracts.module';
 
 @Module({
-  imports: [LeadsModule, AccountsModule, ContactsModule, OpportunitiesModule, CrmtasksModule, ContractsModule, InteractionsModule]
+  imports: [LeadsModule, AccountsModule, ContactsModule, OpportunitiesModule, CrmtasksModule, ContractsModule, InteractionsModule],
+  // En exportant ces modules, leurs contrôleurs et services deviennent disponibles pour le reste de l'application.
+  exports: [LeadsModule, AccountsModule, ContactsModule, OpportunitiesModule, CrmtasksModule, ContractsModule, InteractionsModule],
 })
 export class CrmModule {}
